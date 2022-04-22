19794d5bbd90e9fa80a6a0ec73149ada3730e03e
ari-12-2000 <76164766+ari-12-2000@users.noreply.github.com>
2022-04-22 22:47:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const port=process.env.PORT || 3000
 //const io= require('socket.io')(port)// passing function tothe port. socket.io will run on port
 const server= require("http").createServer(app)
 const io= require('socket.io')(server)
+const path = require("path")
+const static_path= path.join(__dirname,"./public")
 // app.listen(port, ()=>{
 //     console.log(`Server is running at port no ${port}`);
 // })
 //io.on("connection",socket =>{console.log(socket.id)}) // callback function will run everytime a client connects to our server and socket instance will be given to them
-app.use(express.static('public')) // or //app.use(express.static(__dirname + '/public'))
+app.use(express.static(static_path)) // or //app.use(express.static(__dirname + '/public'))
 app.get('/',(req,res)=>{
     res.sendFile(`${__dirname}/index.html`) //eita or res.sendFile(__dirname+"/index.html")
 })
